c59cf0be285c213db533b6c0cbc4d9feb8062533
sarasin <saracsin26@gmail.com>
2019-09-30 20:35:12 -0300

    mock test

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import requests
 
 def get_temperature(lat, lng):
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
-    url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
-    reponse = requests.get(url)
-    data = reponse.json()
+    url = f'https://api.darksky.net/forecast/{key}/{lat},{lng}'
+    response = requests.get(url)
+    data = response.json()
     temperature = data.get('currently').get('temperature')
-    print(temperature)
     if not temperature:
         return
     return int((temperature - 32) * 5.0 / 9.0)
-
-print(get_temperature(-14.235004, -51.92528))
--- test_main.py
+++ test_main.py
@@ -1,9 +1,22 @@
 from main import get_temperature
 import pytest
+import requests
+from unittest.mock import Mock
 
-def test_get_temperature_by_lat_lng():
+class MockResponse:
+    @staticmethod
+    def json():
+        return {"currently": {"temperature": 62}}
+
+@pytest.fixture
+def mock_response(monkeypatch):
+    def mock_get(*args, **kwargs):
+        return MockResponse.json()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+def test_get_temperature_by_lat_lng(mock_response):
     result = get_temperature(-14.235004, -51.92528)
-    # with pytest.raises(16):
-    #     get_temperature(-14.235004, -51.92528)
-    assert result == 36
-    # raise NotImplementedError
+    assert result == 16
+
+test_get_temperature_by_lat_lng(mock_response)
